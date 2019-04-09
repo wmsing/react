4dbcc5bcb0455bd38a746359a0b22c760cde516a
“Sing <singwaiman@gmail.com>
2019-04-09 19:37:30 +0800

    Updates

---
+++ precache-manifest.96be3b1953916b7353779e4e6f05dfa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2ceef7fcbd35532b1b5",
+    "url": "/react/static/js/runtime~main.b2ceef7f.js"
+  },
+  {
+    "revision": "b2a779ecd9a89f7a1798",
+    "url": "/react/static/js/main.b2a779ec.chunk.js"
+  },
+  {
+    "revision": "46a820051e9743eaf1ab",
+    "url": "/react/static/js/2.46a82005.chunk.js"
+  },
+  {
+    "revision": "b2a779ecd9a89f7a1798",
+    "url": "/react/static/css/main.7cdae12e.chunk.css"
+  },
+  {
+    "revision": "46a820051e9743eaf1ab",
+    "url": "/react/static/css/2.977ca0f0.chunk.css"
+  },
+  {
+    "revision": "e09107a996eb1df24ea48d462b29152d",
+    "url": "/react/index.html"
+  }
+];
